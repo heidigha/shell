f11c8390bcf4046a6fc789cc7c577cebe1f3751b
Heidi Ghafoor <57541395+heidigha@users.noreply.github.com>
2024-08-21 19:00:31 -0400

    answered 8.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -24,7 +24,10 @@ touch file1 file2 file3 file4 file5
 cat dir2/file3
 
 # 6. Delete file4
+ rm file4
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
+rm -r dir4/* dir5/*
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+ls
