f8adc8e22d54ff3d08d0b063412230f5f63153bf
Heidi Ghafoor <57541395+heidigha@users.noreply.github.com>
2024-08-25 16:16:18 -0400

    Update assignment.sh
    Compete assignment and update incorrect ansswers

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,7 +14,7 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-touch dir2/ file1 file2 file3 file4 file5
+ttouch dir2/ file1.txt file2.txt file3.txt file4.txt file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3
 echo "Hello world" > dir2/file3.txt
@@ -23,7 +23,8 @@ echo "Hello world" > dir2/file3.txt
 cat dir2/file3.txt
 
 # 6. Delete file4
-rm -i dir2/file4
+rm -i dir2/file4.txt
+
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 cd ..
 rm -r dir4 dir5
